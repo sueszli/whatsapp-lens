bb6acaaceec0fe1e3864c0cdaf11872d6862ace1
sueszli <jabaryyahya@gmail.com>
2024-10-30 20:47:08 +0100

    up

--- src/1_feature_extractor.py
+++ src/1_feature_extractor.py
@@ -1,6 +1,5 @@
-from pathlib import Path
-import json
 import glob
+from pathlib import Path
 from types import SimpleNamespace
 
 import pandas as pd
@@ -10,7 +9,7 @@ from tqdm import tqdm
 from utils import get_current_dir
 
 
-def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+def preprocess(df: pd.DataFrame, path: Path) -> pd.DataFrame:
     df = df.copy()
 
     # type conversion
@@ -22,14 +21,22 @@ def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     df = df[df["author"] != "server"]
     df = df[~df["message"].str.contains("New messages will disappear from this chat 24 hours after they're sent, except when kept. Tap to change.")]
     assert len(df["author"].unique()) == 2
-    
-    # replace media messages with placeholder (for freq analysis)
+
+    # replace media messages with placeholder
     placeholder = "<MEDIA OMITTED>"
     df["message"] = df["message"].str.replace("<Media omitted>", placeholder)
     df["message"] = df["message"].str.replace("(file attached)", placeholder)
     df["message"] = df["message"].str.replace("location: https://maps.google.com/?q", placeholder)
     poll_mask = df["message"].str.contains("|".join(["POLL:", "OPTION:", "votes"]), case=False)  # polls
     df.loc[poll_mask, "message"] = placeholder
+
+    # set author as "author"
+    authors = df["author"].unique()
+    filename = Path(path).name.split(".")[0]
+    sim0 = sum([1 for c in authors[0].lower() if c in filename.lower()]) / len(authors[0])
+    sim1 = sum([1 for c in authors[1].lower() if c in filename.lower()]) / len(authors[1])
+    authorname = authors[1] if sim0 > sim1 else authors[0]
+    df["author"] = df["author"].cat.rename_categories({authorname: "author"})
     return df
 
 
@@ -48,26 +55,6 @@ def get_language(df: pd.DataFrame) -> str:
     return lang
 
 
-
-def get_partner_name(df: pd.DataFrame, path: Path) -> str:
-    df = df.copy()
-    authors = df["author"].unique()
-    authors = [a for a in authors if a != "server"]
-    assert len(authors) == 2
-
-    filename = Path(path).name.split(".")[0]
-
-    fst_sim = sum([1 for c in authors[0].lower() if c in filename.lower()]) / len(authors[0])
-    snd_sim = sum([1 for c in authors[1].lower() if c in filename.lower()]) / len(authors[1])
-
-    return authors[0] if fst_sim > snd_sim else authors[1]
-
-
-def get_frequency(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.copy()
-
-    pass
-
 if __name__ == "__main__":
     args = SimpleNamespace(
         inputpath=get_current_dir().parent / "data" / "robustness",
@@ -77,18 +64,28 @@ if __name__ == "__main__":
     path = glob.glob(str(args.inputpath / "*.csv"))[0]
 
     df = pd.read_csv(path)
-    df = preprocess(df)
+    df = preprocess(df, path)
 
-    language = get_language(df)
-    partnername = get_partner_name(df, path)
+    # author_freqs = get_freq_analysis(df, author=True)
+    # partner_freqs = get_freq_analysis(df, author=False)
+
+    author_name = "author"
+    partner_name = df["author"].unique()[0] if df["author"].unique()[1] == "author" else df["author"].unique()[1]
+
+    results = {
+        "conversation_language": get_language(df),
+    }
 
     # guess gender from author and partner
     # guess age from author and partner
 
     # drop placeholder messages after frequency analysis
 
-
     # get frequency metadata
+    # - total message ratio
+    # - average word count
+    # - media count
+    # - emoji count
 
     # get topics metadata
 
