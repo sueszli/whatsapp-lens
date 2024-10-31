e13a8c87e7bd128c4d809dae72d4d92f70d6aeef
sueszli <jabaryyahya@gmail.com>
2024-10-31 03:55:06 +0100

    up

--- src/2_feature_extractor.py
+++ src/2_feature_extractor.py
@@ -280,8 +280,6 @@ if __name__ == "__main__":
     os.makedirs(weightspath, exist_ok=True)
 
     for path in tqdm(glob.glob(str(args.inputpath / "*.csv"))):
-        path = glob.glob(str(args.inputpath / "*.csv"))[0]
-
         df = pd.read_csv(path)
         df = preprocess(df)
         author_name = get_author_name(df, path)
@@ -290,12 +288,12 @@ if __name__ == "__main__":
             "conversation_language": get_language(df),
             "author_name": author_name,
             "partner_name": df["author"].unique()[0] if df["author"].unique()[0] != author_name else df["author"].unique()[1],
-            # **get_monthly_sentiments(df, author_name, sample_size=1_000),
-            # **get_monthly_toxicity(df, author_name, sample_size=1_000),
-            # **get_gender_stats(df, author_name),
-            # "topic_diversity": get_topic_diversity_score(df),
-            # **get_freq_stats(df, author_name),
-            # "embeddings": get_embedding(df),
+            **get_monthly_sentiments(df, author_name, sample_size=1_000),
+            **get_monthly_toxicity(df, author_name, sample_size=1_000),
+            **get_gender_stats(df, author_name),
+            "topic_diversity": get_topic_diversity_score(df),
+            **get_freq_stats(df, author_name),
+            "embeddings": get_embedding(df),
         }
 
         with open(args.outputpath / f"results.csv", "a") as f:
