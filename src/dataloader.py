278af08da7aa916c71712584eb17ca234e2ae5aa
sueszli <jabaryyahya@gmail.com>
2024-10-31 05:17:55 +0100

    up

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,1 +1,14 @@
-# load data into results from huggingface
+from huggingface_hub import snapshot_download
+import os
+from utils import get_current_dir
+
+resultspath = get_current_dir().parent / "data" / "results"
+os.makedirs(resultspath, exist_ok=True)
+
+dataset = snapshot_download(
+    repo_id="sueszli/whatsapp-lens",
+    repo_type="dataset",
+    local_dir=resultspath,
+    allow_patterns="*.csv",
+    token=True,
+)
